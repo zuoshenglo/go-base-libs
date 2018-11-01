3cf42e7d62d9df55588227c7ea1a10c411f4bf99
zuosheng <151362201@qq.com>
2018-11-01 16:18:56 +0800

    add struct BaseDingDing

--- dingding.go
+++ dingding.go
@@ -1,56 +1,56 @@
 package go_base_libs
 
 import (
-	"encoding/json"
-	"net/http"
-	"crypto/tls"
 	"bytes"
-	"io/ioutil"
-	log "github.com/cihub/seelog"
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/cihub/seelog"
 )
 
 //叮叮告警是一个接口，下面应该对应多种格式
 
-var Dingding = & dingding{}
+var Dingding = &dingding{}
 
 type dingding struct {
 	PostStruct postStruct
-	Url string
-
+	Url        string
 }
 
 type postStruct struct {
 	Msgtype string `json:"msgtype"`
-	Text text `json:"text"`
+	Text    text   `json:"text"`
 }
 
 type text struct {
 	Content string `json:"content"`
 }
 
-func (d *dingding) SetUrl(url string) * dingding{
+func (d *dingding) SetUrl(url string) *dingding {
 	d.Url = url
 	return d
 }
 
-func (d * dingding) SetContent(content string ) * dingding{
+func (d *dingding) SetContent(content string) *dingding {
 	d.PostStruct.Text.Content = content
 	return d
 }
 
-func (d * dingding)  SetMsgtype(msgType string) * dingding{
+func (d *dingding) SetMsgtype(msgType string) *dingding {
 	d.PostStruct.Msgtype = msgType
 	return d
 }
 
-func (d * dingding) SendDingDingAlert(content string) string {
+func (d *dingding) SendDingDingAlert(content string) string {
 
 	d.SetContent(content)
 
-	postJson,err := json.Marshal(d.PostStruct)
+	postJson, err := json.Marshal(d.PostStruct)
 
-	if err != nil{
+	if err != nil {
 		log.Error("转换结构体为json格式失败，请检查程序", err)
 		return fmt.Sprintf("转换结构体为json格式失败，请检查程序:%s", err)
 	}
@@ -62,7 +62,7 @@ func (d * dingding) SendDingDingAlert(content string) string {
 	req, err := http.NewRequest("POST", d.Url, bytes.NewBuffer(postJson))
 	req.Close = true
 	req.Header.Set("Content-Type", "application/json")
-	client := &http.Client{Transport:tr}
+	client := &http.Client{Transport: tr}
 	resp, herr := client.Do(req)
 	if herr != nil {
 		log.Error("给叮叮发送告警信息失败", herr)
@@ -78,3 +78,9 @@ func (d * dingding) SendDingDingAlert(content string) string {
 //dd.setMsgtype("text") # 叮叮告警结构的类型
 //dd.setUrl(ServiceConf.Elasticsearch.Dingurl) # 叮叮的地址
 //dd.setContent(content) # 告警的内容
+
+type BaseDingDing struct {
+	MsgContent string `json:"msgContent"`
+	Url        string `json:"url"`
+	MsgType    string `json:"msgType"`
+}
