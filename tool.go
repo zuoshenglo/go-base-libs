824cac77dc4c5852a93e858ca3e7059e554b9c5f
zuosheng <151362201@qq.com>
2018-11-05 12:28:27 +0800

    添加去重函数

--- tool.go
+++ tool.go
@@ -17,3 +17,17 @@ func (t *tool) GetCwd() string {
 func (t *tool) JsonStringToStruct() {
 
 }
+
+//去重复
+func (t *tool) RemoveRepByMap(slc []string) []string {
+	result := []string{}
+	tempMap := map[string]byte{} // 存放不重复主键
+	for _, e := range slc {
+		l := len(tempMap)
+		tempMap[e] = 0
+		if len(tempMap) != l { // 加入map后，map长度变化，则元素不重复
+			result = append(result, e)
+		}
+	}
+	return result
+}
