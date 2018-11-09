c57b9d4896eb6b36e80a371005e73fec0f07a0f0
zuosheng <151362201@qq.com>
2018-11-09 11:25:07 +0800

    添加跨域设置

--- tool.go
+++ tool.go
@@ -1,6 +1,7 @@
 package go_base_libs
 
 import (
+	"net/http"
 	"os"
 )
 
@@ -31,3 +32,23 @@ func (t *tool) RemoveRepByMap(slc []string) []string {
 	}
 	return result
 }
+
+//http服务设置跨域， 添加头部
+func (t *tool) SetCrossDomain(w http.ResponseWriter) http.ResponseWriter {
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+	w.Header().Set("Access-Control-Max-Age", "1000")
+	w.Header().Set("Access-Control-Allow-Headers", "AccessKey,Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With,X-Request-Id,X-Server-Addr,AppToken,PicAuth,kbn-version")
+	w.Header().Set("Access-Control-Expose-Headers", "Content-Type,Origin,User-Agent,X-Requested-With,X-Request-Id,X-Server-Addr")
+	return w
+}
+
+//nginx 设置
+// #add_header 'Access-Control-Allow-Origin' '*';
+// add_header 'Access-Control-Allow-Credentials' 'true';
+// add_header 'Access-Control-Allow-Methods' 'GET, POST, PUT, DELETE, OPTIONS, HEAD';
+// add_header 'Access-Control-Allow-Headers' 'content-type,AccessKey,Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With,X-Request-Id,X-Server-Addr,AppToken,PicAuth,kbn-version';
+// add_header 'Access-Control-Expose-Headers' 'Content-Type,Origin,User-Agent,X-Requested-With,X-Request-Id,X-Server-Addr';
+// int To string  string := strconv.Itoa(int)
+// int64 To string string := strconv.FormatInt(int64,10)
